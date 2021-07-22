2855dca60a74f234b78e31a5d5e82c3e32df5457
DVinciusBonin <vinibonin007@gmail.com>
2021-07-22 20:28:01 -0300

    add routes of create, update and list

--- src/routes.ts
+++ src/routes.ts
@@ -5,6 +5,11 @@ const routes = Router();
 
 
 routes.post('/cars', CarController.save);
+
 routes.get('/cars', CarController.getAll);
+
+routes.get('/car', CarController.getCar);
+
+routes.put('/cars/:car_id', CarController.update);
 
 export default routes;
